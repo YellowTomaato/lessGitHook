34d17b96195a05513b504230350e3a3db748eb40
David Shevchenko <david.shevchenko@incode-it.com>
2021-06-30 10:42:53 +0300

    test6

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 const s  = () => {
-  console.log('helo')
+  con
+  sole.log('helo')
 }
 ReactDOM.render(
   <React.StrictMode>
